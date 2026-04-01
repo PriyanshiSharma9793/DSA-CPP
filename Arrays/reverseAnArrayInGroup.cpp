3f91aa5cb1b4010094bcb8a80c7d89db3fecfe2e
PriyanshiSharma9793 <sharmepriyanshi@gmail.com>
2026-04-01 23:06:25 +0530

    modified

---
+++ Arrays/reverseAnArrayInGroup.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+void reversearray(vector <int> &arr,int k){
+    int n= arr.size();
+      for(int i=0;i<n;i+=k){
+   int left = i;
+
+        
+        int right = min(i + k - 1, n - 1);
+
+        while (left < right)  {
+            swap(arr[left++], arr[right--]);
+        }
+    }
+}
+
+int main() {
+  
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8}; 
+    int k = 3; 
+    reversearray(arr, k); 
+
+    for (int num : arr)
+        cout << num << " ";
+
+    return 0;
+}
+
