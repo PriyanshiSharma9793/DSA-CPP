1ac8b3d18311777318486c5cd31e3d73d8ff881c
PriyanshiSharma9793 <sharmepriyanshi@gmail.com>
2026-04-03 00:00:00 +0530

    modified

--- Arrays/reverseAnArrayInGroup.cpp
+++ Arrays/reverseAnArrayInGroup.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 void reversearray(vector <int> &arr,int k){
     int n= arr.size();
-      for(int i=0;i<n;i+=k){
-   int left = i;
-
-        
-        int right = min(i + k - 1, n - 1);
+    for(int i=0;i<n;i+=k){
+    int left = i;
+    int right = min(i + k - 1, n - 1);
 
         while (left < right)  {
             swap(arr[left++], arr[right--]);
@@ -27,4 +25,4 @@ int main() {
 
     return 0;
 }
-
+      
--- Arrays/rotateAnArray.cpp
+++ Arrays/rotateAnArray.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <bits/stdc++.h>
 // void rotateArr(vector<int>& arr, int d) {
 //     int n = arr.size();
 //     for (int i = 0; i < d; i++) {
@@ -29,35 +30,59 @@ using namespace std;
 // }
 
 //[Better Approach] Using Temporary Array - O(n) Time and O(n) Space
-void rotateArr(vector<int>& arr, int d) {
-    int n = arr.size();
+// void rotateArr(vector<int>& arr, int d) {
+//     int n = arr.size();
 
    
-    d %= n;
+//     d %= n;
   
-    vector<int> temp(n);
+//     vector<int> temp(n);
 
     
-    for (int i = 0; i < n - d; i++)
-        temp[i] = arr[d + i];
+//     for (int i = 0; i < n - d; i++)
+//         temp[i] = arr[d + i];
 
     
-    for (int i = 0; i < d; i++)
-        temp[n - d + i] = arr[i];
+//     for (int i = 0; i < d; i++)
+//         temp[n - d + i] = arr[i];
 
-    for (int i = 0; i < n; i++)
-        arr[i] = temp[i];
+//     for (int i = 0; i < n; i++)
+//         arr[i] = temp[i];
+// }
+
+// int main() {
+//     vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+//     int d = 2;
+
+//     rotateArr(arr, d);
+
+    
+//     for (int i = 0; i < arr.size(); i++)
+//         cout << arr[i] << " ";
+
+//     return 0;
+// }
+//[Expected Approach 1] Using Juggling Algorithm - O(n) Time and O(1) Space
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+  
+    d %= n;
+
+    reverse(arr.begin(), arr.begin() + d);
+
+
+    reverse(arr.begin() + d, arr.end());
+ 
+    reverse(arr.begin(), arr.end());
 }
 
 int main() {
     vector<int> arr = { 1, 2, 3, 4, 5, 6 };
     int d = 2;
-
-    rotateArr(arr, d);
-
     
-    for (int i = 0; i < arr.size(); i++)
+  	rotateArr(arr, d);
+  
+    for (int i = 0; i < arr.size(); i++) 
         cout << arr[i] << " ";
-
     return 0;
 }
