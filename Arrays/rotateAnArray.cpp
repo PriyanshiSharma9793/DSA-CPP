cf03a69f736516e91b4c5210271c726b8bed4e32
PriyanshiSharma9793 <sharmepriyanshi@gmail.com>
2026-04-02 23:54:59 +0530

    modify

---
+++ Arrays/rotateAnArray.cpp
@@ -0,0 +1,63 @@
+//[Naive Approach] Rotate one by one 
+//Time Complexity- O(n * d) 
+//Space Complexity-O(1) 
+#include <iostream>
+using namespace std;
+#include <vector>
+// void rotateArr(vector<int>& arr, int d) {
+//     int n = arr.size();
+//     for (int i = 0; i < d; i++) {
+      
+//         int first = arr[0];
+//         for (int j = 0; j < n - 1; j++) {
+//             arr[j] = arr[j + 1];
+//         }
+//         arr[n - 1] = first;
+//     }
+// }
+
+// int main() {
+//     vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+//     int d = 2;
+
+//     rotateArr(arr, d);
+
+//     for (int i = 0; i < arr.size(); i++)
+//         cout << arr[i] << " ";
+
+//     return 0;
+// }
+
+//[Better Approach] Using Temporary Array - O(n) Time and O(n) Space
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+
+   
+    d %= n;
+  
+    vector<int> temp(n);
+
+    
+    for (int i = 0; i < n - d; i++)
+        temp[i] = arr[d + i];
+
+    
+    for (int i = 0; i < d; i++)
+        temp[n - d + i] = arr[i];
+
+    for (int i = 0; i < n; i++)
+        arr[i] = temp[i];
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+    int d = 2;
+
+    rotateArr(arr, d);
+
+    
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
